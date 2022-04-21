f9adca39879e5d46cd9c1c74d813b9c86433c8b3
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-21 15:28:26 +0200

    add contracts

---
+++ src/ErniAcademy.Cache.Contracts/CacheOptions.cs
@@ -0,0 +1,42 @@
+﻿namespace ErniAcademy.Cache.Contracts;
+
+public class CacheOptions : ICacheOptions
+{
+    private TimeSpan? _absoluteExpirationRelativeToNow;
+    private TimeSpan? _slidingExpiration;
+
+    public DateTimeOffset? AbsoluteExpiration { get; set; }
+
+    public TimeSpan? AbsoluteExpirationRelativeToNow
+    {
+        get
+        {
+            return _absoluteExpirationRelativeToNow;
+        }
+        set
+        {
+            if (value <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(AbsoluteExpirationRelativeToNow), value, "The relative expiration value must be positive.");
+            }
+
+            _absoluteExpirationRelativeToNow = value;
+        }
+    }
+
+    public TimeSpan? SlidingExpiration
+    {
+        get
+        {
+            return _slidingExpiration;
+        }
+        set
+        {
+            if (value <= TimeSpan.Zero)
+            {
+                throw new ArgumentOutOfRangeException(nameof(SlidingExpiration), value, "The sliding expiration value must be positive.");
+            }
+            _slidingExpiration = value;
+        }
+    }
+}
---
+++ src/ErniAcademy.Cache.Contracts/ICacheManager.cs
@@ -0,0 +1,64 @@
+﻿namespace ErniAcademy.Cache.Contracts;
+
+public interface ICacheManager
+{
+    /// <summary>
+    /// Get an Item from cache
+    /// </summary>
+    /// <typeparam name="TItem">generic type of the item</typeparam>
+    /// <param name="key">the unique identifier key</param>
+    /// <returns>TItem instance. default(TItem) if not found</returns>
+    TItem Get<TItem>(string key);
+
+    /// <summary>
+    /// Get an Item from cache
+    /// </summary>
+    /// <typeparam name="TItem">generic type of the item</typeparam>
+    /// <param name="key">the unique identifier key</param>
+    /// <returns>Task<TItem>. default(TItem) if not found</returns>
+    Task<TItem> GetAsync<TItem>(string key);
+
+    /// <summary>
+    /// Set an Item into the cache
+    /// </summary>
+    /// <typeparam name="TItem">generic type of the item</typeparam>
+    /// <param name="key">the unique identifier key</param>
+    /// <param name="value">the instance of TItem to be setted into the cache</param>
+    /// <param name="options">the cache options for this item</param>
+    void Set<TItem>(string key, TItem value, ICacheOptions options = null);
+
+    /// <summary>
+    /// Set an Item into the cache
+    /// </summary>
+    /// <typeparam name="TItem">generic type of the item</typeparam>
+    /// <param name="key">the unique identifier key</param>
+    /// <param name="value">the instance of TItem to be setted into the cache</param>
+    /// <param name="options">the cache options for this item</param>
+    Task SetAsync<TItem>(string key, TItem value, ICacheOptions options = null);
+
+    /// <summary>
+    /// Check if there is an item with that key into the cache
+    /// </summary>
+    /// <param name="key">the unique identifier key</param>
+    /// <returns>true if exists, false otherwise</returns>
+    bool Exists(string key);
+
+    /// <summary>
+    /// Check if there is an item with that key into the cache
+    /// </summary>
+    /// <param name="key">the unique identifier key</param>
+    /// <returns>true if exists, false otherwise</returns>
+    Task<bool> ExistsAsync(string key);
+
+    /// <summary>
+    /// Remove an item from cache
+    /// </summary>
+    /// <param name="key">the unique identifier key</param>
+    void Remove(string key);
+
+    /// <summary>
+    /// Remove an item from cache
+    /// </summary>
+    /// <param name="key">the unique identifier key</param>
+    Task RemoveAsync(string key);
+}
