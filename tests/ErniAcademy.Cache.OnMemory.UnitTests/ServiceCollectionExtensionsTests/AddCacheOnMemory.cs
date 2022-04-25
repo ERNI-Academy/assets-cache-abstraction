615ab5d4e7d932703ed71162531718d569638ec0
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-25 11:23:16 +0200

    add unit tests for extensions

---
+++ tests/ErniAcademy.Cache.OnMemory.UnitTests/ServiceCollectionExtensionsTests/AddCacheOnMemory.cs
@@ -0,0 +1,25 @@
+﻿using ErniAcademy.Cache.Contracts;
+using ErniAcademy.Cache.OnMemory.Extensions;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ErniAcademy.Cache.OnMemory.UnitTests.ServiceCollectionExtensionsTests;
+
+public class AddCacheOnMemory
+{
+    [Fact]
+    public void With_valid_options_section_Should_configure_ICacheManager_with_OnMemory_impl()
+    {
+        //Arrange
+        var services = new ServiceCollection();
+        services.AddCacheOnMemory();
+        var provider = services.BuildServiceProvider();
+
+        //Act
+        var actual = provider.GetRequiredService<ICacheManager>();
+
+        //Assert
+        actual.Should().NotBeNull();
+    }
+}
---
+++ tests/ErniAcademy.Cache.StorageBlobs.UnitTests/ServiceCollectionExtensionsTests/AddCacheStorageBlobs.cs
@@ -0,0 +1,41 @@
+﻿using ErniAcademy.Cache.Contracts;
+using ErniAcademy.Cache.StorageBlobs.Extensions;
+using ErniAcademy.Serializers.Contracts;
+using FluentAssertions;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NSubstitute;
+using System.Collections.Generic;
+using Xunit;
+
+namespace ErniAcademy.Cache.StorageBlobs.UnitTests.ServiceCollectionExtensionsTests;
+
+public class AddCacheStorageBlobs
+{
+    private readonly ISerializer _serializer;
+
+    public AddCacheStorageBlobs()
+    {
+        _serializer = Substitute.For<ISerializer>();
+    }
+
+    [Fact]
+    public void With_valid_options_section_Should_configure_ICacheManager_with_StorageBlobs_impl()
+    {
+        //Arrange
+        var configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(new KeyValuePair<string, string>[]{
+                new KeyValuePair<string, string>("StorageBlobs:ConnectionString", "UseDevelopmentStorage=true"),
+                }).Build();
+
+        var services = new ServiceCollection();
+        services.AddCacheStorageBlobs(configuration, _serializer, "StorageBlobs");
+        var provider = services.BuildServiceProvider();
+
+        //Act
+        var actual = provider.GetRequiredService<ICacheManager>();
+
+        //Assert
+        actual.Should().NotBeNull();
+    }
+}
