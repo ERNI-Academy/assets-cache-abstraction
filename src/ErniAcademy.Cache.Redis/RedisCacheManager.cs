cf28f9334dad424e3a8f80c8e4eb699e0410e062
Omar Amalfi Camacho <omar.amalfi@ypsomed.com>
2022-04-25 13:30:12 +0200

    fix code smell The logging message template should not vary between calls to 'LoggerExtensions.Log(ILogger, LogLevel, string?, params object?[])

--- src/ErniAcademy.Cache.Redis/RedisCacheManager.cs
+++ src/ErniAcademy.Cache.Redis/RedisCacheManager.cs
@@ -38,7 +38,7 @@ public class RedisCacheManager : ICacheManager
         var value = await _databaseLazy.Value.StringGetAsync(key);
         var cacheHit = value.HasValue && !value.IsNullOrEmpty;
 
-        _logger.Log(LogLevel.Information, $"Cache get '{key}' hit: {cacheHit}");
+        _logger.Log(LogLevel.Information, $"Cache get '{0}' hit: {1}", key, cacheHit);
 
         if (!cacheHit)
         {
@@ -58,7 +58,7 @@ public class RedisCacheManager : ICacheManager
         var valueStr = _serializer.SerializeToString(value);
         var expiry = (options ?? _defaultOptions).GetExpiration(DateTimeOffset.UtcNow);
 
-        _logger.Log(LogLevel.Information, $"Cache set '{key}' expiry: {expiry?.ToString()}");
+        _logger.Log(LogLevel.Information, $"Cache set '{0}' expiry: {1}", key, expiry?.ToString());
 
         await _databaseLazy.Value.StringSetAsync(key, valueStr, expiry: expiry, flags: CommandFlags.FireAndForget);
     }
